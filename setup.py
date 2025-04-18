a5eaadcf0a41d3aecbb617164e2f1b65d73da293
guaixiao <109229170+guaixiao7720@users.noreply.github.com>
2025-04-19 01:18:18 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="mongohelper",  # 项目名称
     version="0.1.0",  # 版本号
     author="guai_xiao",  # 作者姓名
+    license="GPL-3.0",
     author_email="guai-xiao@hotmail.com",  # 作者邮箱
     description="Repackage pymongo to make it more pythonic",  # 简短描述
     long_description=open("README.md", "r", encoding="utf-8").read(),  # 详细描述
@@ -12,7 +13,7 @@ setup(
     packages = ["mongohelper"],  # 自动发现项目中的包
     classifiers=[
         "Programming Language :: Python :: 3",  # 适用的 Python 版本
-        "License :: OSI Approved :: GNU GPLv3",  # 项目许可证类型
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",  # 项目许可证类型
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",  # 要求的最低 Python 版本
