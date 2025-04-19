e1e1cbfa42e85194a2012d81561fb36d463f7bc1
guaixiao <109229170+guaixiao7720@users.noreply.github.com>
2025-04-20 02:11:33 +0800

    添加operator.collection

--- mongohelper/operator/dict.py
+++ mongohelper/operator/dict.py
@@ -3,13 +3,21 @@ from pymongo.synchronous.database import Database
 
 
 class Dict:
-    def __init__(self,db: Database, list_name: str, mongoid: str):
+    def __init__(self,db: Database, list_name: str, mongoid: str, is_in_list: bool = True):
         self.id: str = mongoid
         self.db: Database = db
         self.list_name: str = list_name
+        self.is_in_list: bool = is_in_list
+        self.key: str = ""
 
     def __getitem__(self, item: str):
-        return self.db[self.list_name].find_one({"_id": ObjectId(self.id)}, {item: 1})[item]
+        if self.is_in_list:
+            return self.db[self.list_name].find_one({"_id": ObjectId(self.id)}, {item: 1})[item]
+        else:
+            return self.db[self.list_name].find_one({"_key": self.key}, {item: 1})[item]
 
     def __setitem__(self, key: str, value):
-        self.db[self.list_name].update_one({"_id" : ObjectId(self.id)}, {"$set" : {key: value}})
+        if self.is_in_list:
+            self.db[self.list_name].update_one({"_id" : ObjectId(self.id)}, {"$set" : {key: value}})
+        else:
+            self.db[self.list_name].update_one({"_key": self.key}, {"$set": {key: value}})
